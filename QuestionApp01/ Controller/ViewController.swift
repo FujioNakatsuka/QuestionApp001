95e797c4d75d814f04429794acf241212d76c502
Fujio Nakatsuka <com.nifty.cbm01799>
2020-02-27 02:08:42 +0900

    Next View Controller追加、パーツ配置、Segueなど、アシスタントエディター表示されないバグ発生、一回閉じて再立ち上げで解消

---
+++ QuestionApp01/ Controller/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  QuestionApp01
+//
+//  Created by 中塚富士雄 on 2020/02/27.
+//  Copyright © 2020 中塚富士雄. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var maxScoreLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        
+        
+    }
+
+    @IBAction func answer(_ sender: Any) {
+    
+        if (sender as AnyObject).tag == 1{
+            
+            
+            
+            
+        }else if (sender as AnyObject).tag == 2{
+            
+            
+            
+        }
+        
+        
+        
+    }
+    
+    
+    
+
+}
+
